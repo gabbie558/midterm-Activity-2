a4ca70a4a3772e0a46480b1926751a058bc9f32e
gabbie558 <enriquezgabriela558@gmail.com>
2024-10-21 09:31:28 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <Container className="mt-5">
       <center>
-        <h1 style={{ marginBottom: '10px',marginTop: '30px' }}>Welcome to My Profile!!</h1>
+        <h1 style={{ marginBottom: '10px',marginTop: '30px' }}>Welcome to My Profile!</h1>
         </center>
       <center><p>I want to be a Graphic Designer!</p></center>
     </Container>
