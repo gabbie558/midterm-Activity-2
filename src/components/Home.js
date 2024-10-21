b0f727aa4ce84c93b49ff450a03c9aa3b80adf8a
gabbie558 <enriquezgabriela558@gmail.com>
2024-10-21 09:25:57 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <center>
         <h1 style={{ marginBottom: '10px',marginTop: '30px' }}>Welcome to My Profile!!</h1>
         </center>
-      <center><p>In this Profile you will be able to see more about me. Enjoy!</p></center>
+      <center><p>I want to be a Graphic designer</p></center>
     </Container>
   );
 }
